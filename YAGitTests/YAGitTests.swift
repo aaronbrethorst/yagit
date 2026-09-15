b3366f5879dd97f749001d71e37660154303b107
Aaron Brethorst <aaron@brethorsting.com>
2026-09-15 14:46:06 -0700

    Initial Commit

---
+++ YAGitTests/YAGitTests.swift
@@ -0,0 +1,19 @@
+//
+//  YAGitTests.swift
+//  YAGitTests
+//
+//  Created by Aaron Brethorst on 9/15/26.
+//
+
+import Testing
+@testable import YAGit
+
+struct YAGitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
